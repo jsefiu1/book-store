ba5b7213ebbd1c92c81d3858480fa2a94e31edcb
jsefiu1 <jonisefiu1@gmail.com>
2024-01-27 02:06:56 +0100

    Update user profile

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -26,6 +26,26 @@ class Cart():
             self.cart[product_id] = int(product_qty)
         self.session.modified = True
 
+    def cart_total(self):
+        # Get product_ids
+        product_ids = self.cart.keys()
+        # lookup those keys in our product database model
+        products = Product.objects.filter(id__in=product_ids)
+        # Get quantities
+        quantities = self.cart
+        total = 0
+        for key, value in quantities.items():
+            # covert key string into int
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        return total
+
+
     def __len__(self):
         return len(self.cart)
     
@@ -38,3 +58,23 @@ class Cart():
     def get_quants(self):
         quantities = self.cart
         return quantities
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+
+        ourcart = self.cart
+        ourcart[product_id] = product_qty
+
+        self.session.modified = True
+
+        thing = self.cart
+        return thing
+    
+    def delete(self, product):
+        product_id = str(product)
+        # delte from dictionary/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+        # save our session after we modified it
+        self.session.modified = True
